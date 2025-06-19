756969caaed4f635884404c1dce4f9c2e38e9a3d
DiogoLuizDeAquino <diogoluiz2110@gmail.com>
2025-06-19 17:34:52 -0300

    Add variables

--- Python starting with the lenguage/mini-game.py
+++ Python starting with the lenguage/mini-game.py
@@ -3,18 +3,21 @@ print('Welcome to this basic divination GAME!!')
 print ('######################################')
 
 secret_number = 73
-attempt_str = input("Find the secret number, take your best shot: ")
+attempt = ""
 
-print("The number what you chose: ", attempt_str)
-attempt = int(attempt_str)
+while (attempt != secret_number):
+    attempt_str = input("Find the secret number, take your best shot: ")
 
-if(secret_number == attempt):
-    print("Well done, you found the secret number!!!")
-else:
-    if(attempt > secret_number):
-        print("Wrong! Your number is too high than the secret number!!")
-    elif(attempt < secret_number):
-        print("Wrong! Your number is too low than the secret number!!")
+    print("The number what you chose is: ", attempt_str)
+    attempt = int(attempt_str)
+
+    if(secret_number == attempt):
+        print("Well done, you found the secret number!!!")
+    else:
+        if(attempt > secret_number):
+            print("Wrong! Your number is too high than the secret number!!")
+        elif(attempt < secret_number):
+            print("Wrong! Your number is too low than the secret number!!")
 
 print("End of the game :)")
 
