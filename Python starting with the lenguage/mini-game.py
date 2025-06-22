75e2ebd152cd1c0f660f389a685443704c03e078
DiogoLuizDeAquino <diogoluiz2110@gmail.com>
2025-06-22 08:47:32 -0300

    Adding a counter to the chances

--- Python starting with the lenguage/mini-game.py
+++ Python starting with the lenguage/mini-game.py
@@ -4,12 +4,15 @@ print ('######################################')
 
 secret_number = 73
 attempt = ""
+chance = 5
+attempt_str = input("Find the secret number. You have 5 chances! Press 'ENTER' to continue ")
 
-while (attempt != secret_number):
-    attempt_str = input("Find the secret number, take your best shot: ")
+while (chance > 0):
+    chance_str = input("Take your best shot:")
+  
 
-    print("The number what you chose is: ", attempt_str)
-    attempt = int(attempt_str)
+    print("The number of chances you have: ", chance)
+    attempt = int(chance_str)
 
     if(secret_number == attempt):
         print("Well done, you found the secret number!!!")
@@ -18,6 +21,11 @@ while (attempt != secret_number):
             print("Wrong! Your number is too high than the secret number!!")
         elif(attempt < secret_number):
             print("Wrong! Your number is too low than the secret number!!")
+
+    chance = chance -1
+    if(chance <= 0):
+            print("Your chances are DONE!!!")
+        
 
 print("End of the game :)")
 
