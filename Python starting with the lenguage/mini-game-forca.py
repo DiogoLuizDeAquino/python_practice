30af2ac95f13c84cd7ba8c823aa1c75bd3cb8993
DiogoLuizDeAquino <diogoluiz2110@gmail.com>
2025-06-23 13:34:06 -0300

    Adding a new game (forca)

---
+++ Python starting with the lenguage/mini-game-forca.py
@@ -0,0 +1,7 @@
+#import mini-game
+#import mini-game-forca
+
+print('#######################################')
+print('Welcome to this basic forca GAME!!')
+print ('######################################')
+
