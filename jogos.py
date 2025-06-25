8696157dbecdcdf629570a263b3bb34992a3d1b9
DiogoLuizDeAquino <diogoluiz2110@gmail.com>
2025-06-25 07:54:17 -0300

    Rename Arqs

--- jogos.py
+++ jogos.py
@@ -1,5 +1,15 @@
+import MiniGameForca
+import MiniGameDivinationDefinitive
+
 print('########################################')
 print('***********Choose a Game!!*************')
 print('########################################')
 
 print('(1) Forca-Game (2) Divination-Game')
+
+game = int(input("What game do you wanna play?: "))
+
+if(game == 1):
+    print("You chose the Forca-Gmme!")
+elif(game == 2):
+    print("You chose the Divination-Game!")
