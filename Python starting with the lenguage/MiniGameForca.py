fc77e13b63b07e493f255ac4326fe2ef970760d7
DiogoLuizDeAquino <diogoluiz2110@gmail.com>
2025-06-27 12:42:12 -0300

    fixing FORCA game logic

--- Python starting with the lenguage/MiniGameForca.py
+++ Python starting with the lenguage/MiniGameForca.py
@@ -2,19 +2,19 @@ def play():
     print('#######################################')
     print('Welcome to this basic forca GAME!!')
     print ('######################################')
-
-def play():
-    print('#######################################')
-    print('Welcome to this basic Hangman GAME!!')
-    print('#######################################')
-
-
+    
     secret_word = "banana"
     hanged = False
     guessed = False
 
     while not hanged and not guessed:
         guess = input("Type a letter: ")
+
+        index = 0
+        for letter in secret_word:
+            if (guess == letter):
+                print("You found the letter {} in the position {}".format(letter, index))
+            index = index + 1
         print("Playing...")
 
     print("End of the game")
